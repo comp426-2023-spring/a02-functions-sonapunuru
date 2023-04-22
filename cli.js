7113b70880944515489441d48b191666eb2e9f49
Sona Punuru <punuru@email.unc.edu>
2023-04-21 23:01:49 -0400

    cliTest3

--- cli.js
+++ cli.js
@@ -1,29 +1,25 @@
 #!/usr/bin/env node
-
-
-//import dependencies
 import minimist from 'minimist';
 import fetch from 'node-fetch';
 import moment from 'moment-timezone';
 
 
 //process arguments
-var agrs = minimist(process.argv.slice(2));
+var args = minimist(process.argv.slice(2));
 
 
-//create the help text
+//create help info
 if (args.h) {
-	Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-    		-h            Show this help message and exit.
-   		-n, -s        Latitude: N positive; S negative.
-    		-e, -w        Longitude: E positive; W negative.
-    		-z            Time zone: uses tz.guess() from moment-timezone by default.
-    		-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    		-j            Echo pretty JSON from open-meteo API and exit.
-			process.exit(0);
+	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit`);
+	process.exit(0);
 }
 
-//extract system timezone
 const timezone = moment.tz.guess();
 
 
@@ -32,22 +28,23 @@ const latitude = args.n || args.s * -1;
 const longitude = args.e || args.w * -1;
 
 
-//weather data
+//get weather data
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=weathercode,temperature_2m_max,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&timezone=' + timezone);
 
+
+//get data 
 const data = await response.json();
 
 
-//allow user to log out
+//allowing user to log out
 if (args.j) {
 	console.log(data);
 	process.exit(0);
 }
 
 
-//define days according to minimist's output
+//set arg.d or default is tomorrow
 let days;
-
 if (args.d == null) {
 	days = 1;
 } else {
@@ -55,15 +52,16 @@ if (args.d == null) {
 }
 
 
-//create weather messages
+//messages for sunny and rainy weather 
 let weather = "";
 if (data.daily.precipitation_hours[days] > 0) {
-	weather += "Expect rain! ";
+	weather += "It looks like there might be rain!";
 } else {
-	weather += "It will be sunny!";
+	weather += "It's going to be bright and sunny!";
 }
 
-//append the appropriate string to the weather message
+
+//create correct string depending on day
 if (days == 0) {
 	weather += "today";
 } else if (days > 1) {
@@ -72,5 +70,5 @@ if (days == 0) {
 	weather += "tomorrow.";
 }
 
-//push weather message to console
+//push message to console
 console.log(weather);
